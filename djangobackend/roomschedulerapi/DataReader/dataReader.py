ca6c0d81cbe0f41794f78f2fd2423a799c70daa7
Hank Rugg <hankrugg2@gmail.com>
2024-02-06 15:59:22 -0700

    Modified format

--- djangobackend/roomschedulerapi/DataReader/dataReader.py
+++ djangobackend/roomschedulerapi/DataReader/dataReader.py
@@ -7,47 +7,52 @@ Author: Hank Rugg
 '''
 
 import pandas as pd
+
+
 # from models.roomschedulerapi import Course, Building, Floor, Room
 
-# load course data
-data = pd.read_csv('2024SpingAllClassSchedule.csv')
+class DataReader(object):
 
-# load class data
-classData = pd.read_csv('ClassroomCapacities.csv')
+    def __init__(self):
+        # load course data
+        self.courseData = pd.read_csv('2024SpingAllClassSchedule.csv')
+        # load classroom data
+        self.classData = pd.read_csv('ClassroomCapacities.csv')
 
-# load classes
-for c in range(len(data)):
-    Course(Course=data['COURSE_SECTIONS_ID'].iloc[c],
-          Section=data['SECTION_STATUS'].iloc[c],
-          StartDate=data['SEC_START_DATE'].iloc[c],
-          EndDate=data['SEC_END_DATE'].iloc[c],
-          Subject=data['SEC_SUBJECT'].iloc[c],
-          CourseNumber=data['SEC_COURSE_NO'].iloc[c],
-          ShortTitle=data['SEC_SHORT_TITLE'].iloc[c],
-          MinCredits=data['SEC_MIN_CRED'].iloc[c],
-          StartTime=data['CSM_START_TIME'].iloc[c],
-          EndTime=data['CSM_END_TIME'].iloc[c],
-          Monday=data['CSM_MONDAY'].iloc[c],
-          Tuesday=data['CSM_TUESDAY'].iloc[c],
-          Wednesday=data['CSM_WEDNESDAY'].iloc[c],
-          Thursday=data['CSM_THURSDAY'].iloc[c],
-          Friday=data['CSM_FRIDAY'].iloc[c],
-          Saturday=data['CSM_SATURDAY'].iloc[c],
-          Sunday=data['CSM_SUNDAY'].iloc[c],
-          Building=data['CSM_BUILDING'].iloc[c],
-          Room=data['CSM_ROOM'].iloc[c],
-          InstructionMethod=data['CSM_INSTR_METHOD'].iloc[c],
-          Faculty=data['SEC_FACULTY_INFO'].iloc[c],
-          StudentEnrollment=data['STUDENTS_AND_RESERVED_SEATS'].iloc[c],
-          StudentCapacity=data['SEC_CAPACITY'].iloc[c],
-          Waitlist=data['WAITLIST'].iloc[c].iloc[c],
-          ChargeAmount=data['CHARGE_AMT_1'].iloc[c],
-          NumberOfWeeks=data['SEC_NO_WEEKS'].iloc[c],
-          ).save()
+    def loadData(self):
+        # load classes
+        for c in range(len(self.courseData)):
+            Course(Course=self.courseData['COURSE_SECTIONS_ID'].iloc[c],
+                   Section=self.courseData['SECTION_STATUS'].iloc[c],
+                   StartDate=self.courseData['SEC_START_DATE'].iloc[c],
+                   EndDate=self.courseData['SEC_END_DATE'].iloc[c],
+                   Subject=self.courseData['SEC_SUBJECT'].iloc[c],
+                   CourseNumber=self.courseData['SEC_COURSE_NO'].iloc[c],
+                   ShortTitle=self.courseData['SEC_SHORT_TITLE'].iloc[c],
+                   MinCredits=self.courseData['SEC_MIN_CRED'].iloc[c],
+                   StartTime=self.courseData['CSM_START_TIME'].iloc[c],
+                   EndTime=self.courseData['CSM_END_TIME'].iloc[c],
+                   Monday=self.courseData['CSM_MONDAY'].iloc[c],
+                   Tuesday=self.courseData['CSM_TUESDAY'].iloc[c],
+                   Wednesday=self.courseData['CSM_WEDNESDAY'].iloc[c],
+                   Thursday=self.courseData['CSM_THURSDAY'].iloc[c],
+                   Friday=self.courseData['CSM_FRIDAY'].iloc[c],
+                   Saturday=self.courseData['CSM_SATURDAY'].iloc[c],
+                   Sunday=self.courseData['CSM_SUNDAY'].iloc[c],
+                   Building=self.courseData['CSM_BUILDING'].iloc[c],
+                   Room=self.courseData['CSM_ROOM'].iloc[c],
+                   InstructionMethod=self.courseData['CSM_INSTR_METHOD'].iloc[c],
+                   Faculty=self.courseData['SEC_FACULTY_INFO'].iloc[c],
+                   StudentEnrollment=self.courseData['STUDENTS_AND_RESERVED_SEATS'].iloc[c],
+                   StudentCapacity=self.courseData['SEC_CAPACITY'].iloc[c],
+                   Waitlist=self.courseData['WAITLIST'].iloc[c].iloc[c],
+                   ChargeAmount=self.courseData['CHARGE_AMT_1'].iloc[c],
+                   NumberOfWeeks=self.courseData['SEC_NO_WEEKS'].iloc[c],
+                   ).save()
 
-# load classroom data
-for c in range(len(classData)):
-      Room(Building=classData['Building'].iloc[c],
-           RoomNumber=classData['Room'].iloc[c],
-           Capacity=classData['Max Capacity'].iloc[c],
-           ).save()
+        # load classroom data
+        for c in range(len(self.classData)):
+            Room(Building=self.classData['Building'].iloc[c],
+                 RoomNumber=self.classData['Room'].iloc[c],
+                 Capacity=self.classData['Max Capacity'].iloc[c],
+                 ).save()
