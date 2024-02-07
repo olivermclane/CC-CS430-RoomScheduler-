4b80ad14863ede65c47d3abea0b0d26cfd4ea89c
Oliver McLane <olivermclane@Thor.local>
2024-02-07 14:23:27 -0700

    Uploading database schema, changing file structure to support incoming react changes

---
+++ django_react_roomscheduler/roomschedulerapi/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+
+class Building(models.Model):
+    building_id = models.AutoField(primary_key=True)
+
+
+class Floor(models.Model):
+    floor_id = models.AutoField(primary_key=True)
+    floor_name = models.CharField(max_length=50)
+    building_id = models.ForeignKey(Building, on_delete=models.CASCADE)
+
+
+class Classroom(models.Model):
+    classroom_id = models.AutoField(primary_key=True)
+    class_room_number = models.IntegerField(default=0, max_length=3)
+    total_seats = models.IntegerField(default=0)
+    width_of_room = models.IntegerField(default=0)
+    length_of_room = models.IntegerField(default=0)
+    projectors = models.IntegerField(default=0)
+    microphone_system = models.BooleanField(default=0)
+    blueray_player = models.BooleanField(default=False)
+    laptop_hdmi = models.BooleanField(default=False)
+    zoom_camera = models.BooleanField(default=False)
+    document_camera = models.BooleanField(default=False)
+    storage = models.BooleanField(default=False)
+    movable_chairs = models.BooleanField(default=False)
+    printer = models.BooleanField(default=False)
+    piano = models.BooleanField(default=False)
+    stereo_system = models.BooleanField(default=False)
+    total_tv = models.IntegerField(default=0)
+    sinks = models.IntegerField(default=0)
+    notes = ArrayField(models.CharField(max_length=200, blank=False))
+    floor_id = models.ForeignKey(Floor, on_delete=models.CASCADE)
+
+
+class Course(models.Model):
+    course_id = models.AutoField(primary_key=True)
+    classroom_id = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    instructor = models.CharField
+    first_day = models.DateField()
+    last_day = models.DateField()
+    course_name = models.CharField(max_length=100)
+    term = ArrayField(models.CharField(max_length=10, blank=True))
+    credits = models.IntegerField(default=0)
+    course_cap = models.IntegerField(default=0)
+    waitlist_cap = models.IntegerField(default=0)
+    course_total = models.IntegerField(default=0)
+    waitlist_total = models.IntegerField(default=0)
+    enrollment_total = models.IntegerField(default=0)
+    course_level = models.CharField(max_length=3)
+    monday = models.BooleanField(default=False)
+    tuesday = models.BooleanField(default=False)
+    wednesday = models.BooleanField(default=False)
+    thursday = models.BooleanField(default=False)
+    friday = models.BooleanField(default=False)
+    saturday = models.BooleanField(default=False)
+    sunday = models.BooleanField(default=False)
