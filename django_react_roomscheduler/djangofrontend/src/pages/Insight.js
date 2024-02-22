e6e4fd9f8a008b70de34fd5a8fef2d3154f75f41
Oliver McLane <olivermclane@Thor.local>
2024-02-21 21:56:22 -0700

    Updated location of charts

--- django_react_roomscheduler/djangofrontend/src/pages/Insight.js
+++ django_react_roomscheduler/djangofrontend/src/pages/Insight.js
@@ -2,7 +2,9 @@ import React from "react";
 import UsageChart from "../components/UsageChart";
 import DailyScheduleInsight from "../components/TimeBarChart";
 import MostFreqProf from "../components/MostFreqProf";
-
+/*
+ * Charts made via: https://apexcharts.com
+ */
 export default function Insight() {
     return (
         <div className="flex flex-1 space-x-10">
